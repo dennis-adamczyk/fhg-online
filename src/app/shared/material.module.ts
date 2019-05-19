5962b9b1008a8a76844219991f475ee2347c1966
dennis-adamczyk <ich@dennis-adamczyk.de>
2019-05-19 13:29:16 +0200

    Add angular material

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
+
+const materialComponents = [MatButtonModule];
+
+@NgModule({
+  imports: materialComponents,
+  exports: materialComponents
+})
+export class MaterialModule {}
